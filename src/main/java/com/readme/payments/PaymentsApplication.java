29f6fea4d15045e77f2114255280460d0952078c
Ju-Yeongmin <drodudals1@gmail.com>
2023-05-01 14:19:04 +0900

    [CREATE] eureka / yml setting

--- src/main/java/com/readme/payments/PaymentsApplication.java
+++ src/main/java/com/readme/payments/PaymentsApplication.java
@@ -2,8 +2,10 @@ package com.readme.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentsApplication {
 
 	public static void main(String[] args) {
