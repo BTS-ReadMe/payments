cfdd669d763860acf9d176f8f4cc16b371f8ed3c
logwns <logwns@gmail.com>
2023-05-29 10:54:02 +0900

    [UPDATE] #BTS-95 : modify kafka server - 로컬에서 ec2 카프카로 값을 변경했습니다

--- src/main/java/com/readme/payments/config/KafkaProducerConfig.java
+++ src/main/java/com/readme/payments/config/KafkaProducerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -15,11 +16,13 @@ import org.springframework.kafka.core.ProducerFactory;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${bootstrap_servers_config}")
+    private String BOOTSTRAP_SERVERS_CONFIG;
+
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> properties = new HashMap<>();
-//        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.18.0.3:9092");
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
