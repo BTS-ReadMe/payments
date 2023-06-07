1560e1986f47d3e65b0db1d319e12b70273e81bb
logwns <logwns@gmail.com>
2023-06-07 17:50:25 +0900

    [CREATE] #BTS-160 : 구매한 에피소드 목록 조회 - 구매한 에피소드들을 볼 수 있게 했습니다

---
+++ src/main/java/com/readme/payments/payments/dto/EpisodeNovelDto.java
@@ -0,0 +1,12 @@
+package com.readme.payments.payments.dto;
+
+import lombok.Getter;
+
+@Getter
+public class EpisodeNovelDto {
+    private String episodeTitle;
+    private String novelTitle;
+    private Long novelId;
+    private int grade;
+    private String thumbnail;
+}
